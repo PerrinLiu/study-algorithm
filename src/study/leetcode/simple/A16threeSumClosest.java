f88824ab4357568e266cced1b70716fcd97b3963
LLPY <309829291@qq.com>
2024-04-29 17:13:33 +0800

    feat: 新增力扣16题解法

--- src/study/leetcode/simple/A16threeSumClosest.java
+++ src/study/leetcode/simple/A16threeSumClosest.java
@@ -41,7 +41,7 @@ public class A16threeSumClosest {
 //            -104 <= target <= 104
     public static void main(String[] args) {
         List<Object> param = new ArrayList<>();
-        int[] nums = {4,0,5,-5,3,3,0,-4,-5};
+        int[] nums = {-5, -5, -4, 0, 0, 3, 3, 4};
         int target = -2;
         param.add(nums);
         param.add(target);
@@ -65,11 +65,16 @@ class threeSumClosest extends TimingTemplate {
         int tab = Integer.MAX_VALUE;
         int source;
         for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     source = nums[i] + nums[j] + nums[k];
                     int num = source > target ? source - target : target - source;
-                    if (num > tab) continue;
+                    if (num > tab) {
+                        continue;
+                    }
                     tab = Math.min(tab, num);
                     res = tab == num ? source : res;
                     if (tab == 0) {
@@ -88,28 +93,49 @@ class threeSumClosest2 extends TimingTemplate {
     protected Object method(Object obj) {
         List<Object> params = (List<Object>) obj;
         int[] nums = (int[]) params.get(0);
-        int target = (int) params.get(1);
+        int target = (int) params.get(1);   //{-5,-5,-4,-3,0,0,3,3,4};{-5,-5,-4,-3,0,0,3,3,4};{-5,-5,-4,-3,0,0,3,3,4};
         Arrays.sort(nums);
+        int len = nums.length;
         int res = Integer.MAX_VALUE;
-        int tab = Integer.MAX_VALUE;
-        int i = 0;
-        while ((i+2)<nums.length){
-            int count1=1;
-            int count2=2;
-            while(nums[i]==nums[i+count1] && i+count1+1<nums.length){
-                count1++;
+        int difference = Integer.MAX_VALUE;
+        // 枚举 a
+        for (int i = 0; i < len; ++i) {
+            // 保证和上一次枚举的元素不相等
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-            while(nums[i]==nums[i+count2] && i+count2+1<nums.length){
-                count2++;
+            // 使用双指针枚举 b 和 c
+            int j = i + 1, k = len - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                // 如果和为 target 直接返回答案
+                if (sum == target) {
+                    return target;
+                }
+                // 根据差值来更新答案
+                int num = sum > target ? sum - target : target - sum;
+                if (difference > num) {
+                    res = sum;
+                    difference = num;
+                }
+                if (sum > target) {
+                    // 如果和大于 target，移动 c 对应的指针
+                    int k0 = k - 1;
+                    // 移动到下一个不相等的元素
+                    while (j < k0 && nums[k0] == nums[k]) {
+                        --k0;
+                    }
+                    k = k0;
+                    continue;
+                }
+                // 如果和小于 target，移动 b 对应的指针
+                int j0 = j + 1;
+                // 移动到下一个不相等的元素
+                while (j0 < k && nums[j0] == nums[j]) {
+                    ++j0;
+                }
+                j = j0;
             }
-            int source = nums[i] + nums[i+count1] + nums[i+count2];
-            int num = source >target? source - target :target-source;
-            if(tab<=num){
-                break;
-            }
-            tab = num;
-            res = source;
-            i++;
         }
         return res;
     }
