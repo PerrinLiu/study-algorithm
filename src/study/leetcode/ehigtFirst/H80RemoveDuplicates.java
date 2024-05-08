0bd8384d86f1a8ea1a31a3f0790b72fd52a45ffb
LLPY <309829291@qq.com>
2024-05-08 17:33:44 +0800

    feat: 完成力扣80题,还可优化

--- src/study/leetcode/ehigtFirst/H80RemoveDuplicates.java
+++ src/study/leetcode/ehigtFirst/H80RemoveDuplicates.java
@@ -1,5 +1,10 @@
 package study.leetcode.ehigtFirst;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+
 /**
  * h80删除重复项
  *
@@ -7,32 +12,12 @@ package study.leetcode.ehigtFirst;
  * @date 2024/05/08
  */
 public class H80RemoveDuplicates {
-    //80. 删除有序数组中的重复项 II
-//    中等
-//            相关标签
-//    相关企业
+    //80. 删除有序数组中的重复项 II  中等
 //    给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
 //
 //    不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 //
-//
-//
-//    说明：
-//
-//    为什么返回数值是整数，但输出的答案是数组呢？
-//
-//    请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
-//
-//    你可以想象内部操作如下:
-//
-//    // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
-//    int len = removeDuplicates(nums);
-//
-//// 在函数里修改输入数组对于调用者是可见的。
-//// 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
-//for (int i = 0; i < len; i++) {
-//        print(nums[i]);
-//    }
+
 //
 //
 //    示例 1：
@@ -48,11 +33,34 @@ public class H80RemoveDuplicates {
 //
 //
 //    提示：
-//
 //            1 <= nums.length <= 3 * 104
 //            -104 <= nums[i] <= 104
 //    nums 已按升序排列
     public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int i = removeDuplicates(nums);
+        System.out.println(i);
+    }
 
+    public static int removeDuplicates(int[] nums){
+        TreeMap<Integer, Integer> hashMap = new TreeMap<>();
+        for (int num : nums) {
+            if(!hashMap.containsKey(num)){
+                hashMap.put(num,1);
+                continue;
+            }
+            if( hashMap.get(num)<2){
+                hashMap.put(num,2);
+            }
+        }
+        int index = 0;
+        for (Integer num : hashMap.keySet()) {
+            Integer number = hashMap.get(num);
+            for (int i = 0; i < number; i++) {
+                nums[index++] = num;
+            }
+        }
+
+        return index;
     }
 }
