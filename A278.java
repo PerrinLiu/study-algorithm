01dd8c82be6f7508f68745334035cf3e8eac5fe8
LLPY <309829291@qq.com>
2024-05-14 17:20:05 +0800

    完成力扣278题

---
+++ A278.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=java
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+import java.math.BigInteger;
+
+public class A278 extends VersionControl {
+    public static void main(String[] args) {
+        int firstBadVersion = new A278().firstBadVersion(2126753390);
+        System.out.println(firstBadVersion);
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+
+/**
+ * 278.第一个错误的版本
+ */
+class VersionControl {
+
+    public boolean isBadVersion(int version) {
+        int bad = 1702766719;
+        return version >= bad;
+    }
+}
+// @lc code=end
