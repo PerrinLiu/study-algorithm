212af8cca60280740f266b35fc62b586ed943da7
LLPY <309829291@qq.com>
2024-05-21 09:34:57 +0800

    完成2769

---
+++ src/study/leetcode/BFirst/2769.找出最大的可达成数字.java
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=2769 lang=java
+ *
+ * [2769] 找出最大的可达成数字
+ */
+
+// @lc code=start
+class Solution2769 {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
+// @lc code=end
