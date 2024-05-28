4f144b655dcb1267f26957a1b135d4641ba57232
LLPY <309829291@qq.com>
2024-05-28 09:54:09 +0800

    完成2951

--- 2951.找出峰值.java
+++ 2951.找出峰值.java
@@ -5,10 +5,26 @@
  */
 
 // @lc code=start
-class Solution {
-    public List<Integer> findPeaks(int[] mountain) {
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 3, 8, 5 };
+        System.out.println(new Solution().findPeaks(arr));
+    }
+
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < mountain.length - 1; i++) {
+            int left = mountain[i - 1];
+            int right = mountain[i + 1];
+            if (mountain[i] > left && mountain[i] > right) {
+                list.add(i);
+            }
+        }
+        return list;
     }
 }
 // @lc code=end
-
