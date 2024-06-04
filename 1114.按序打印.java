1d18897266c2f62941c13373bd932ab4be29d2fe
LLPY <309829291@qq.com>
2024-06-04 15:48:08 +0800

    完成1114

---
+++ 1114.按序打印.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=1114 lang=java
+ *
+ * [1114] 按序打印
+ */
+
+// @lc code=start
+class Foo {
+
+    private final Object object = new Object();
+    private volatile int flag = 1;
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (object) {
+            while (flag != 1) {
+                object.wait();
+            }
+            printFirst.run();
+            flag = 2;
+            object.notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (object) {
+            while (flag != 2) {
+                object.wait();
+            }
+            printSecond.run();
+            flag = 3;
+            object.notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (object) {
+            while (flag != 3) {
+                object.wait();
+            }
+        }
+        printThird.run();
+    }
+}
+// @lc code=end
