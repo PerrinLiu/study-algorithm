c0f202f10cd10f2420dfa949b4a46cec8dcfa66e
LLPY <309829291@qq.com>
2024-06-05 16:44:46 +0800

    完成64

--- 64.最小路径和.java
+++ 64.最小路径和.java
@@ -5,16 +5,46 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution64 {
     public static void main(String[] args) {
         int[][] grid = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
-        System.out.println(new Solution().minPathSum(grid));
+        System.out.println(new Solution64().minPathSum(grid));
     }
 
     public int minPathSum(int[][] grid) {
-        int res = 0;
+        int rows = grid.length, columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[rows - 1][columns - 1];
+    }
 
-        return res;
+    public int minPathSum1(int[][] grid) {
+        int gLen = grid.length;
+        int gArrLen = grid[0].length;
+        for (int i = 0; i < gLen; i++) {
+            for (int j = 0; j < gArrLen; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                else if (i == 0)
+                    grid[i][j] = grid[i][j - 1] + grid[i][j];
+                else if (j == 0)
+                    grid[i][j] = grid[i - 1][j] + grid[i][j];
+                else
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[gLen - 1][gArrLen - 1];
     }
 }
 // @lc code=end
