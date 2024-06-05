83d7bf5b64373c59e5631a739de09997e040195b
LLPY <309829291@qq.com>
2024-06-05 15:54:43 +0800

    完成64

---
+++ 64.最小路径和.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=java
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+class Solution {
+    public static void main(String[] args) {
+        int[][] grid = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+        System.out.println(new Solution().minPathSum(grid));
+    }
+
+    public int minPathSum(int[][] grid) {
+        int res = 0;
+
+        return res;
+    }
+}
+// @lc code=end
