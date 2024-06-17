e4ade441ea40da582f4e4d8749a627f2cf78498a
Perrin <309829291@qq.com>
2024-06-17 17:35:12 +0800

    完成438

---
+++ 438.找到字符串中所有字母异位词.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=java
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution438 {
+    public static void main(String[] args) {
+        String s = "abab";
+        String p = "ab";
+        System.out.println("\n" + new Solution438().findAnagrams(s, p));
+    }
+
+    public List<Integer> findAnagrams1(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
+        char[] arr = p.toCharArray();
+        Arrays.sort(arr);
+        String valueOf = String.valueOf(arr);
+        for (int i = 0; i <= sLen - pLen; i++) {
+            char[] temp = s.substring(i, i + pLen).toCharArray();
+            Arrays.sort(temp);
+            if (valueOf.equals(String.valueOf(temp))) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
+        char[] arr = p.toCharArray();
+        Arrays.sort(arr);
+        String valueOf = String.valueOf(arr);
+        for (int i = 0; i <= sLen - pLen; i++) {
+            char[] temp = s.substring(i, i + pLen).toCharArray();
+            Arrays.sort(temp);
+            if (valueOf.equals(String.valueOf(temp))) {
+                res.add(i);
+                char c = s.charAt(res.get(res.size() - 1));
+                while (i + pLen < s.length() && c == s.charAt(i + pLen)) {
+                    c = s.charAt(res.get(res.size() - 1));
+                    res.add(i++);
+                }
+
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
--- src/study/leetcode/DFirst/42.接雨水.java
+++ src/study/leetcode/DFirst/42.接雨水.java
@@ -5,28 +5,26 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution42 {
     public static void main(String[] args) {
         int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-        System.out.println(new Solution().trap(height));
+        System.out.println(new Solution42().trap(height));
     }
 
     public int trap(int[] height) {
-        int ans = 0;
+        int res = 0;
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
         while (left < right) {
             leftMax = Math.max(leftMax, height[left]);
             rightMax = Math.max(rightMax, height[right]);
             if (height[left] < height[right]) {
-                ans += leftMax - height[left];
-                ++left;
+                res += leftMax - height[left++];
             } else {
-                ans += rightMax - height[right];
-                --right;
+                res += rightMax - height[right--];
             }
         }
-        return ans;
+        return res;
     }
 }
 // @lc code=end
